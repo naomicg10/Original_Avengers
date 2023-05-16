1d804cc7eee012811e5b4dbbc022cac6cb4cc21f
naomicg10 <naomicg4@gmail.com>
2023-05-17 01:34:06 +0200

    Excepciones y fotos

---
+++ original.avengers.ncamuna.daw/src/main/java/excepciones/ContraseñaInvalidaExcepcion.java
@@ -0,0 +1,9 @@
+package excepciones;
+
+public class ContraseñaInvalidaExcepcion extends Exception {
+	public ContraseñaInvalidaExcepcion(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
